7c75ed4ebb769bb10d59fe1f8345b76966e81145
ResJay <562514042@qq.com>
2017-06-27 04:15:01 +0800

    增加搜索功能

--- RandomHC.js
+++ RandomHC.js
@@ -13,11 +13,14 @@ function RandomHC(NAME,INDEX) {
     }
     if(NAME=="少女"){
         name="shaonvmanhua"
-        console.log("**********少女H漫数量巨大请稍后.......*********")
+        console.log("**********少女漫数量巨大请稍后.......*********")
     }
     var p = new Promise(
         function (resolve, reject) {
-            if(INDEX){resolve(INDEX)}
+            if(typeof INDEX=="number"){resolve(INDEX)}
+            else {
+
+            }
             var index = 0
             if (name == "shenshimonhua") {
                 index = 6
@@ -27,9 +30,9 @@ function RandomHC(NAME,INDEX) {
             }
             var ARR = []
             superagent.get("http://www.lfkumh.com/" + name + "/list_" + index + "_1.html").end(function (err, res) {
-                console.log("*************正在读取总H漫数....请稍后....***********************")
-                var g = 0
+                console.log("*************正在读取总本子数....请稍后....***********************")
                 var $ = cheerio.load(res.text)
+                var g = 0
                 var page = 0
                 var timer = 0
                 $(".pageinfo strong").each(function (i, ele) {
@@ -44,20 +47,26 @@ function RandomHC(NAME,INDEX) {
                     else {
                         g == page
                         clearInterval(timer)
-                        console.log(name + "一共有H漫" + ARR.length + "部")
+                        console.log(name + "一共有本子" + ARR.length + "部")
                         console.log("准备开始下载")
                         var x = ARR[Math.round(ARR.length * Math.random())]
-                        fs.appendFile("1.txt",ARR)
                         resolve(x)
                     }
                     superagent.get("http://www.lfkumh.com/" + name + "/list_" + index + "_" + g + ".html").end(function (err, res) {
-                        if(err){return}
+                        if(err){
+                            console.log("**请稍后**")
+                            return}
                         if(res.text=="undefined"){
                             g++
                             console.log("**请稍后**")
                             return}
                         var $ = cheerio.load(res.text)
                         $(".piclist a").each(function (i, ele) {
+                            var bookname=$(this).attr("title")
+                            if(bookname.indexOf(INDEX)!==-1){
+                                console.log("找到本子:"+$(this).attr("title"))
+                                resolve($(this).attr("href").replace(/[^0-9]/ig, ""))
+                            }
                             ARR.push($(this).attr("href").replace(/[^0-9]/ig, ""))
                         })
 
@@ -69,7 +78,7 @@ function RandomHC(NAME,INDEX) {
     )
 
     p.then(function (arr) {
-        console.log("正在下载" + arr)
+        console.log("正在下载的番号为:" + arr)
         function Haitai(ht) {
             var i = 2
             var page = 0
@@ -95,7 +104,7 @@ function RandomHC(NAME,INDEX) {
                     }
                     ;
                     page = z.replace(/[^0-9]/ig, "")
-                    console.log("******************这部漫画一共有"+page+"页***************")
+                    console.log("******************这本子一共有"+page+"页***************")
                     timer = setInterval(
                         function () {
                             if (i < page) {
@@ -130,10 +139,11 @@ function RandomHC(NAME,INDEX) {
         Haitai(arr)
     })
 }
-  //
 //  NAME参数只有两个 "绅士"  或者 "少女"  NAME为字符串格式
-//  INDEX参数为漫画番号  不输入默认为随机漫画   输错番号会报错
-RandomHC("少女",9082)
+//  INDEX参数为漫画番号 或者 漫画名  不输入默认为随机漫画   输错番号会报错
+//  番号名为数字 例如:RandomHC("少女",9333)
+//  搜索名为字符串 例如:RandomHC("少女","ちゃんに見せられ")
+ exports.download=RandomHC
 
 /*exports.RandomHC=function (NAME,INDEX) {
     var name = NAME
